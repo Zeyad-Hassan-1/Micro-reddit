48fe26b1c4240a8268a972895b37b0e643445db7
Zeyad Hasan <studying.zezo@gmail.com>
2025-02-17 23:47:24 +0200

    Add validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_16_163756) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_17_213739) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "post_id"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,5 +39,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_16_163756) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
